fa7b5f2aec864c7056081b90010335291ced4653
Beth Ruff <beth.ruff2@gmail.com>
2020-04-26 14:58:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,24 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do 
+    @recipes = Recipe.all
+    erb :index
+  end 
+  
+  get 'recipes/new' do 
+    erb :new
+  end 
+ 
+  post '/recipes' do 
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cooktime])
+  end 
+  
+  get 'recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    erb :show 
+  end 
+  
+  
 
 end
