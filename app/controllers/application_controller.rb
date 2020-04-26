0988a7ffa42ed8ab8542a997ee3f643bb2377dfc
Beth Ruff <beth.ruff2@gmail.com>
2020-04-26 15:29:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   get '/recipes/:id/edit' do 
-    @recipe = Recipe.find_by_id(:id => params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end 
   
