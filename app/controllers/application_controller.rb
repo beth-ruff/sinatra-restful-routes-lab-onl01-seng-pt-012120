7b62d8210dabea0d00ee335fbdca36fa6fee9ea0
Beth Ruff <beth.ruff2@gmail.com>
2020-04-26 15:36:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,7 @@ class ApplicationController < Sinatra::Base
  
   post '/recipes' do 
     @recipe = Recipe.create(params[:name], params[:ingredients], params[:cook_time])
+    @recipe.save
     redirect "/recipes/#{@recipe.id}"
   end 
   
